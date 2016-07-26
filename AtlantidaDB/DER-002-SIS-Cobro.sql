ccbd8706960f274a28a1df16b2e9ef9b35f1a57e
Angel Prado <angelbrunn@gmail.com>
2016-07-26 00:24:17 -0300

    AGPB - Database content

---
+++ AtlantidaDB/DER-002-SIS-Cobro.sql
@@ -0,0 +1,86 @@
+CREATE TABLE Cobro ( 
+	idCobro int NOT NULL,
+	idPresupuesto int NOT NULL,
+	monto float NOT NULL,
+	nroFactura int NOT NULL,
+	ventaCancelada bit,
+	fechaCobro datetime NOT NULL
+)
+;
+
+CREATE TABLE PresupuestoCobros ( 
+	idCobros int NOT NULL,
+	idPresupuesto int NOT NULL
+)
+;
+
+CREATE TABLE Ventas ( 
+	idVentas  int NOT NULL,
+	idCobros  int NOT NULL,
+	fechaVenta date
+)
+;
+
+CREATE TABLE VentasVoucher ( 
+	idCobro int NOT NULL,
+	idVoucher char(10) NOT NULL
+)
+;
+
+CREATE TABLE Voucher ( 
+	idVoucher char(10) NOT NULL,
+	fechaVencimiento datetime NOT NULL,
+	fechaCreacion datetime NOT NULL,
+	monto float NOT NULL,
+	usado bit DEFAULT 0 NOT NULL
+)
+;
+
+
+ALTER TABLE Cobro
+	ADD CONSTRAINT UQ_Cobro_idCobro UNIQUE (idCobro)
+;
+
+ALTER TABLE PresupuestoCobros
+	ADD CONSTRAINT UQ_PresupuestoCobros_idPresupuesto UNIQUE (idPresupuesto)
+;
+
+ALTER TABLE PresupuestoCobros
+	ADD CONSTRAINT UQ_PresupuestoCobros_idCobros UNIQUE (idCobros)
+;
+
+ALTER TABLE Ventas
+	ADD CONSTRAINT UQ_Cobro_idCobro UNIQUE (idCobros)
+;
+
+ALTER TABLE VentasVoucher
+	ADD CONSTRAINT UQ_CobrosVoucher_idVoucher UNIQUE (idVoucher)
+;
+
+ALTER TABLE VentasVoucher
+	ADD CONSTRAINT UQ_CobrosVoucher_idCobro UNIQUE (idCobro)
+;
+
+ALTER TABLE Voucher
+	ADD CONSTRAINT UQ_Voucher_idVoucher UNIQUE (idVoucher)
+;
+
+ALTER TABLE Cobro ADD CONSTRAINT PK_Cobros 
+	PRIMARY KEY CLUSTERED (idCobro)
+;
+
+ALTER TABLE PresupuestoCobros ADD CONSTRAINT PK_PresupuestoCobros 
+	PRIMARY KEY CLUSTERED (idCobros, idPresupuesto)
+;
+
+ALTER TABLE Ventas ADD CONSTRAINT PK_Ventas 
+	PRIMARY KEY CLUSTERED (idVentas, idCobros)
+;
+
+ALTER TABLE VentasVoucher ADD CONSTRAINT PK_CobrosVoucher 
+	PRIMARY KEY CLUSTERED (idCobro, idVoucher)
+;
+
+ALTER TABLE Voucher ADD CONSTRAINT PK_Voucher 
+	PRIMARY KEY CLUSTERED (idVoucher)
+;
---
+++ AtlantidaDB/DER-003-SIS-Pagos.sql
@@ -0,0 +1,79 @@
+CREATE TABLE CuentaCorriente ( 
+	idCuentaCorriente int NOT NULL,
+	nroCuentaCorriente int NOT NULL,
+	totalDeuda float DEFAULT 0 NOT NULL,
+	fechaCancelacionDeuda datetime NOT NULL
+)
+;
+
+CREATE TABLE CuentaCorrientePago ( 
+	idPago int NOT NULL,
+	idCuentaCorriente int NOT NULL
+)
+;
+
+CREATE TABLE Divisa ( 
+	idDivisa int NOT NULL,
+	descripcion char(10),
+	valorActual float NOT NULL
+)
+;
+
+CREATE TABLE Pago ( 
+	idPago int NOT NULL,
+	confirmacionPagoRealizado bit DEFAULT 0 NOT NULL,
+	fechaPago datetime,
+	idPresupuesto int NOT NULL
+)
+;
+
+CREATE TABLE presupuestoDivisa ( 
+	idPresupuesto int NOT NULL,
+	idDivisa int NOT NULL
+)
+;
+
+
+ALTER TABLE CuentaCorriente
+	ADD CONSTRAINT UQ_CuentaCorriente_idCuentaCorriente UNIQUE (idCuentaCorriente)
+;
+
+ALTER TABLE CuentaCorrientePago
+	ADD CONSTRAINT UQ_CuentaCorrientePago_idCuentaCorriente UNIQUE (idCuentaCorriente)
+;
+
+ALTER TABLE CuentaCorrientePago
+	ADD CONSTRAINT UQ_CuentaCorrientePago_idPago UNIQUE (idPago)
+;
+
+ALTER TABLE Divisa
+	ADD CONSTRAINT UQ_Divisa_idDivisa UNIQUE (idDivisa)
+;
+
+ALTER TABLE presupuestoDivisa
+	ADD CONSTRAINT UQ_presupuestoDivisa_idDivisa UNIQUE (idDivisa)
+;
+
+ALTER TABLE presupuestoDivisa
+	ADD CONSTRAINT UQ_presupuestoDivisa_idPresupuesto UNIQUE (idPresupuesto)
+;
+
+ALTER TABLE CuentaCorriente ADD CONSTRAINT PK_CuentaCorriente 
+	PRIMARY KEY CLUSTERED (idCuentaCorriente)
+;
+
+ALTER TABLE CuentaCorrientePago ADD CONSTRAINT PK_CuentaCorrientePago 
+	PRIMARY KEY CLUSTERED (idPago, idCuentaCorriente)
+;
+
+ALTER TABLE Divisa ADD CONSTRAINT PK_Divisa 
+	PRIMARY KEY CLUSTERED (idDivisa)
+;
+
+ALTER TABLE Pago ADD CONSTRAINT PK_Pago 
+	PRIMARY KEY CLUSTERED (idPago)
+;
+
+ALTER TABLE presupuestoDivisa ADD CONSTRAINT PK_presupuestoDivisa 
+	PRIMARY KEY CLUSTERED (idPresupuesto, idDivisa)
+;
